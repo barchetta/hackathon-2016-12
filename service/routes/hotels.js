d852bc3d6e91bc59e23ef5ee823d6968827d310d
Joe Di Pol <joe.dipol@oracle.com>
2016-12-14 16:53:21 -0800

    Hook up hotel listing to google apis

--- service/routes/hotels.js
+++ service/routes/hotels.js
@@ -7,12 +7,23 @@ var HttpProxyAgent = require('http-proxy-agent');
 var HttpsProxyAgent = require('https-proxy-agent');
 var myapp = require('../app');
 
+var locationRawData = null;
+
 /* GET hotels listing. */
 router.get('/', function(req, res, next) {
 
-    // HTTP endpoint for the proxy to connect to
-    var endpoint = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=' +
-            myapp.apiKey;
+    getLocation('1600+Amphitheatre+Parkway,+Mountain+View,+CA', res, getListing);
+
+    return;
+});
+
+// getLocation converts a physical address to lat,lng then passes that
+// to getListing to generate the hotel listing near that address
+getLocation = function(address, res, callback) {
+
+    var endpoint = 'https://maps.googleapis.com/maps/api/geocode/json?' +
+        'address=' + address +
+        '&key=' + myapp.apiKey;
 
     console.log('attempting to GET %j', endpoint);
     var opts = url.parse(endpoint);
@@ -21,22 +32,65 @@ router.get('/', function(req, res, next) {
     var agent = new HttpsProxyAgent(myapp.http_proxy);
     opts.agent = agent;
 
-    https.get(opts, function (myres) {
+    var clientRequest = https.get(opts, function (myres) {
         console.log('"response" event!', myres.headers);
-        myres.pipe(process.stdout);
+        //myres.pipe(process.stdout);
         myres.setEncoding('utf8');
         let rawData = '';
         myres.on('data', (chunk) => rawData += chunk);
         myres.on('end', () => {
             try {
+                // Parse JSON to get lat,lng of address
                 let parsedData = JSON.parse(rawData);
-                console.log(parsedData);
+                let lat = parsedData.results[0].geometry.location.lat;
+                let lng = parsedData.results[0].geometry.location.lng;
+                let location = lat  + "," + lng;
+                console.log(location);
+
+                // Invoke callback
+                callback(location, res)
             } catch (e) {
                 console.log(e.message);
             }
-            res.send(rawData);
         });
     });
-});
+};
+
+
+// Given a lication in the form of "lat,lng" return a list of hotels
+getListing = function(location, res) {
+
+     var endpoint = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?' +
+        'location=' + location +
+        '&radius=5000' +
+        '&type=lodging' +
+        '&key=' + myapp.apiKey;
+
+     console.log('attempting to GET %j', endpoint);
+     var opts = url.parse(endpoint);
+
+     // create an instance of the `HttpProxyAgent` class with the proxy server information
+     var agent = new HttpsProxyAgent(myapp.http_proxy);
+     opts.agent = agent;
+
+     https.get(opts, function (myres) {
+         console.log('"response" event!', myres.headers);
+         myres.pipe(process.stdout);
+         myres.setEncoding('utf8');
+         let rawData = '';
+         myres.on('data', (chunk) => rawData += chunk);
+         myres.on('end', () => {
+             var parsedData = "";
+             try {
+                 parsedData = JSON.parse(rawData);
+                 console.log(parsedData);
+             } catch (e) {
+                 console.log(e.message);
+             }
+             res.send(parsedData);
+         });
+     });
+
+}
 
 module.exports = router;
